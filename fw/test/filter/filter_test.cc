89363d99c0c18e4495782bf8afade6b237e1873b
Tim Drysdale <timothy.d.drysdale@gmail.com>
2022-04-28 20:50:41 +0100

    test (fw): update filter_test

--- fw/test/filter/filter_test.cc
+++ fw/test/filter/filter_test.cc
@@ -29,7 +29,29 @@ TEST(Filter, NilFilter) {
 
 
 
+TEST(Filter, LowPassFilter) {
 
+  Filter f = Filter(0.5,1);
+  
+  long y;
+
+  y = f.value();
+
+  EXPECT_EQ(y, 0) << "y = " << y;
+
+  y = f.update(1);
+
+  EXPECT_EQ(y, 1) << "y = " << y;
+
+  y = f.update(0);
+
+  EXPECT_EQ(y, 0) << "y = " << y;
+
+  y = f.update(1);
+  
+  EXPECT_EQ(y, 1) << "y = " << y;
+ 
+}
 
 
 
