95d73f91e8d07aaa788183b6496dabef02a9c283
david <dprydereid@gmail.com>
2024-04-23 13:11:06 +0100

    ui (fix): popuphelp closes on lose focus (blur)

--- ui/default/src/components/elements/PopupHelp.vue
+++ ui/default/src/components/elements/PopupHelp.vue
@@ -2,7 +2,7 @@
 
 <template>
 <div id='popup-help'>
-    <button type='button' class='button-toolbar button-secondary' id='help-button' @click='toggleHelp' aria-label="popup help">
+    <button type='button' class='button-toolbar button-secondary' id='help-button' @blur="closeHelp" @click='openHelp' aria-label="popup help">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-square" viewBox="0 0 16 16">
             <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
             <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
@@ -16,7 +16,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title">Help</h5>
-                    <button type='button' :class="getDarkTheme ? 'btn-close' : 'btn-close btn-close-white'" id='close-button' @click='toggleHelp'></button>
+                    <button type='button' :class="getDarkTheme ? 'btn-close' : 'btn-close btn-close-white'" id='close-button' aria-label="Close" @click='closeHelp'></button>
                 </div>
                 <div class="modal-body">
                     <slot id='popup-text'></slot>
@@ -24,12 +24,14 @@
                 </div>
 
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-danger" @click="toggleHelp">Close</button>
+                    <button type="button" class="btn btn-danger" @click="closeHelp">Close</button>
                 </div>
             </div>
             </div>
         </div>
-        </transition>
+    </transition>
+
+    
     
 </div>
 </template>
@@ -70,25 +72,12 @@ export default {
   },
   
   methods: {
-      toggleHelp(){
-          //this.setPopupWidth();
-          //this.setPopupHeight();
-          this.popup_showing = !this.popup_showing;
+      openHelp(){
+          this.popup_showing = true;
       },
-    //   setPopupWidth(){
-    //       let div = document.getElementById(this.parentDivID);
-    //       if(div != null){
-    //           this.popupWidth = div.clientWidth - 50;
-    //       } 
-    //   },
-    //   setPopupHeight(){
-    //       let div = document.getElementById(this.parentDivID);
-    //       if(div != null){
-    //           this.popupHeight = div.clientHeight - 50;
-    //       } 
-    //   }
-     
-      
+      closeHelp(){
+        this.popup_showing = false;
+      }
   }
 }
 </script>
